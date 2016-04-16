2cd3e1171a142d2cacc330bde1763d55b19e806f
Fedor Garin <fedor.garin@gmail.com>
2016-04-15 22:15:59 -0400

    add ability to import or take a photo

--- audio-nomad-ios/MainViewController.swift
+++ audio-nomad-ios/MainViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, UINavigationControllerDelegate {
+    
+    // MARK: - Variables
+    
+    
+    // MARK: - Initializers
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +25,46 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Styles
+    
+    
+    // MARK: - Methods
+    
+    // MARK: - Actions
+    
+    @IBAction func takePhoto(sender: AnyObject) {
 
-    /*
-    // MARK: - Navigation
+        let imagePickerActionSheet = UIAlertController(title: "Snap/Upload Photo", message: nil, preferredStyle: .ActionSheet)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            let cameraButton = UIAlertAction(title: "Take Photo", style: .Default) { (alert) -> Void in
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = .Camera
+                self.presentViewController(imagePicker, animated: true, completion: nil)
+            }
+            imagePickerActionSheet.addAction(cameraButton)
+        }
+
+        let libraryButton = UIAlertAction(title: "Choose Existing", style: .Default) { (alert) -> Void in
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .PhotoLibrary
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        imagePickerActionSheet.addAction(libraryButton)
+
+        let cancelButton = UIAlertAction(title: "Cancel", style: .Cancel) { (alert) -> Void in
+        
+        }
+        imagePickerActionSheet.addAction(cancelButton)
+
+        presentViewController(imagePickerActionSheet, animated: true, completion: nil)
     }
-    */
 
+}
+
+
+extension MainViewController: UIImagePickerControllerDelegate {
+    
 }
